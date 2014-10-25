0db58c56e0fa3d4d39cc9bf1786b6d3badc949c2
Chris Sanchez <csanchez505@gmail.com>
2014-10-25 15:15:42 -0600

    third Commit, with actionlistener

--- LayoutPractice.java
+++ LayoutPractice.java
@@ -4,42 +4,46 @@ import java.awt.*;
 import java.awt.event.*;
 
 
-public class LayoutPractice implements ActionListener{
+public class LayoutPractice extends JFrame implements ActionListener{
     
     public LayoutPractice(){
-         buildGUI();
+         createWindow();
     }
-    
+
     public static void main(String[] args){
       LayoutPractice lObj = new LayoutPractice();        
     }
     
-   private static void buildGUI(){
+   private void createWindow(){
       // button01.addActionListener(this);
-           
+       
        JFrame frame = new JFrame("Layout Practice");
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        
        
-       JPanel panel01 = new JPanel(new BorderLayout());
-       panel01.add(new JButton("Button"),BorderLayout.PAGE_START );
+       JPanel panel01 = new JPanel();
+       
+       JButton b1 = new JButton("Button"); 
+       b1.addActionListener(this);
+       panel01.add(b1);
+       b1.setPreferredSize(new Dimension(100, 50));
+       
+     //  panel01.add(new JButton("Button"),BorderLayout.PAGE_START );
+       
+       getContentPane().add(panel01);
+       pack();
+       
        frame.setContentPane(panel01);
        
-       JButton b1 = new JButton(); 
        
        // This is an empty content area in the frame
-       JLabel label01 = new JLabel("Button Clicks");
+       JLabel label01 = new JLabel();
        label01.setPreferredSize(new Dimension(500, 500));
-       
-       
-       
+       //label01.setLocation(0,100);
        
        frame.getContentPane().add(label01, BorderLayout.CENTER);
        frame.pack();
        frame.setVisible(true);
-       
-       b1.setPreferredSize(new Dimension(50, 50));
-       panel01.add(b1);
        
        frame.addWindowListener(new WindowAdapter(){
            public void windowClosing(WindowEvent e){
@@ -50,7 +54,7 @@ public class LayoutPractice implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent arg0) {
-        // TODO Auto-generated method stub
+        System.out.println("Button Clicked");
     }
 
 }
