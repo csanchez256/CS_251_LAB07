c40f4ec5800cac2c9fb5ed1a222e9dd8a4fa62b4
Chris Sanchez <csanchez505@gmail.com>
2014-10-27 21:18:07 -0600

    7th commit, buttons, paint component, action listeners

--- LayoutPractice.java
+++ LayoutPractice.java
@@ -9,40 +9,50 @@
 
 import javax.swing.*;
 import javax.swing.border.Border;
-
 import java.awt.*;
 import java.awt.event.*;
 
-public class LayoutPractice extends JPanel  {
+public class LayoutPractice extends JPanel {
 
     static int numClicks;
     private static JTextField messageText = new JTextField();
     static LayoutPractice clickedamount = new LayoutPractice();
-    
-    public LayoutPractice(){
+
+    public void setClicks(int value) {
+        this.numClicks = value;
     }
-    
-    public void setClicks(int value){
-       this.numClicks = value;
-    }
-    public int getClicks(){
+
+    public int getClicks() {
         return this.numClicks;
     }
+
+    /* 
+     * This method is part of the Java Swing toolkit. it
+     * Paints circles
+     */
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
-        /*(x,y, width, height)*/
+
+        /* (x,y, width, height) */
         g.drawOval(50, 50, 400, 400);
         g.setColor(Color.black);
         g.fillOval(50, 50, 400, 400);
-        
+
         g.drawOval(75, 75, 350, 350);
         g.setColor(Color.cyan);
         g.fillOval(75, 75, 350, 350);
+
+        g.drawOval(100, 100, 300, 300);
+        g.setColor(Color.BLACK);
+        g.fillOval(100, 100, 300, 300);
+
+        g.drawOval(125, 125, 250, 250);
+        g.setColor(Color.cyan);
+        g.fillOval(125, 125, 250, 250);
     }
 
     /**
-     * Creates the window itself 
+     * Creates the window and panel
      */
     private static void createWindow() {
 
@@ -53,18 +63,19 @@ public class LayoutPractice extends JPanel  {
 
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-        panel.setPreferredSize(new Dimension(500,500));
+        panel.setPreferredSize(new Dimension(500, 500));
 
         JPanel borderedPanel = new LayoutPractice();
-        Border border = BorderFactory.createTitledBorder("Click Buttons");
+        Border border = BorderFactory
+                .createTitledBorder("Click Buttons for FUN!");
         borderedPanel.setBorder(border);
 
         panel.add(borderedPanel);
-        
+
         frame.getContentPane().add(panel);
         frame.pack();
         frame.setVisible(true);
-        /*set the background of the panel*/
+        /* set the background of the panel */
         frame.getContentPane().setBackground(Color.white);
     }
 
@@ -73,9 +84,9 @@ public class LayoutPractice extends JPanel  {
      * @param contentPane
      */
     private static void addComponentsToPane(Container contentPane) {
-        /*Sets the layout manager for this container. */
+        /* Sets the layout manager for this container. */
         contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
-        counterButton("Button 1", contentPane);
+        counterButton("Click Counter", contentPane);
         dialogButton("Click for Dialog", contentPane);
     }
 
@@ -84,35 +95,40 @@ public class LayoutPractice extends JPanel  {
      * @param text
      * @param container
      */
-    private static void counterButton(final String text, final Container container) {
+    private static void counterButton(final String text,
+            final Container container) {
         JButton button = new JButton(text);
-        final JLabel label01 = new JLabel("Number of clicks: "+clickedamount.getClicks());
+        final JLabel label01 = new JLabel("Number of clicks: "
+                + clickedamount.getClicks());
         button.setAlignmentX(Component.LEFT_ALIGNMENT);
-        container.add(button);
         container.add(label01);
+        container.add(button);
 
-        /*Listens to action events, in this case the clicking
-         * of a button
+        /*
+         * Listens to action events, in this case the clicking of the
+         * Counter Button
          */
-        button.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 numClicks++;
-            clickedamount.setClicks(numClicks);
-           label01.setText("clicks: "+clickedamount.getClicks());
+                clickedamount.setClicks(numClicks);
+                label01.setText("Number of clicks: "
+                        + clickedamount.getClicks());
             }
         });
     }
-    
+
     private static void dialogButton(String text, Container container) {
         JButton button = new JButton(text);
         button.setAlignmentX(Component.LEFT_ALIGNMENT);
         container.add(button);
-        /*Listens to action events, in this case the clicking
-         * of a button
+        /*
+         * Listens to action events, in this case the clicking of a button
          */
-        button.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                  JOptionPane.showMessageDialog(messageText, "You clicked "+numClicks+" times");
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(messageText, "You clicked "
+                        + numClicks + " times");
             }
         });
     }
